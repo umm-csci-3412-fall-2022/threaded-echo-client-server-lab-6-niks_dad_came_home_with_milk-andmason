d61787679bcf68cef7ccacddad8c624c61fc6fae
Mason Eischens <eisch115@morris.umn.edu>
2022-11-01 16:03:24 -0500

    added threads to EchoClient!

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,23 +1,72 @@
 package echoserver;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class EchoClient {
-	public static final int PORT_NUMBER = 6013;
+	public static final int portNumber = 6013;
 
 	public static void main(String[] args) throws IOException {
-		EchoClient client = new EchoClient();
-		client.start();
-	}
+		String hostname;
+		if(args.length == 0) {
+			hostname = "127.0.0.1";
+		} else {
+			hostname = args[0];
+		}
 
-	private void start() throws IOException {
-		Socket socket = new Socket("localhost", PORT_NUMBER);
-		InputStream socketInputStream = socket.getInputStream();
-		OutputStream socketOutputStream = socket.getOutputStream();
+		try {
+			// Create a one way connection to the server
+			Socket socker = new Socket(hostname, portNumber);
+			// Read the standard input stream
+                        // What the user is inputting
+                        // Reads a single char and returns the ASCII table value of that char
+                        // Returns -1 if no character has been read(end i think)
+			
+			Thread inThread = new Thread(new Runnable() {
+				@Override
+				public void run(){
+				  try{
+					int character; 
+					OutputStream output = socker.getOutputStream();
+					while((character = System.in.read()) != -1){
+						output.write((byte)character);
+						output.flush();
+					}
+				    } catch (IOException ce) {
+		      			System.out.println("We were unable to connect to " + hostname);
+      					System.out.println("You should make sure the server is running.");
+				    }
+				}
+			});
+			Thread outThread = new Thread(new Runnable() {
+				@Override 
+				public void run(){
+				   try{	
+					InputStream serverInput = socker.getInputStream();
+					int character;
+					while((character = serverInput.read()) != -1){	
+						System.out.write((char)character);
+					}	
+				   } catch (IOException ce){
+					System.out.println("Something is very, very wrong..");
+				   }				
+				}
+			});
+			inThread.start();
+			outThread.start();
+			inThread.join();
+			outThread.join();
+			System.out.flush();
+			// Close the socket when we're done reading from it
+			socker.close();
 
-		// Put your code here.
+			// Provide some minimal error handling.
+    		} catch (ConnectException ioe) {
+      			System.out.println("We caught an unexpected exception");
+      			System.err.println(ioe);
+    		} catch (InterruptedException ie) {
+			System.out.println("Thread was interrupted");
+		}
+				
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,31 +1,43 @@
 package echoserver;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
+import java.util.*;
 
 public class EchoServer {
-	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+	public static final int portNumber = 6013;
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+	public static void main(String[] args) {
+    		try {
+      			// Start listening on the specified port
+      			ServerSocket sock = new ServerSocket(portNumber);
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+      			// Run forever, which is common for server style services
+      			while (true) {
+        			// Wait until someone connects, thereby requesting a date
+        			Socket client = sock.accept(); // Waits until a client connects
+        			System.out.println("Got a request!");
+				
+				boolean wait = true;
+				while (wait){
+					try {
+					//if (client.getInputStream() = 0) {client.close(); wait = false;}	
+						
+					// Writes the contents of the client input stream to its output stream	
+					client.getOutputStream().write((byte)client.getInputStream().read());
+      					}
+					
+					catch (SocketException se){
+						client.close();
+						wait = false;
+					}
+				}
+				}	
+			
+
+    		// *Very* minimal error handling.
+    		} catch (IOException ioe) {
+     	 		System.out.println("We caught an unexpected exception");
+      			System.err.println(ioe);
+    		}
+  	}
 }
