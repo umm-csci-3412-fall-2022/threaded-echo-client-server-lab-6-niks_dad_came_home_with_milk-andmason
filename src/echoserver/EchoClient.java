9785a1689dde86196956c926671c3f7b1ae2a4a7
Mason Eischens <eisch115@morris.umn.edu>
2022-11-06 14:47:25 -0600

    did some stuff

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -13,51 +13,53 @@ public class EchoClient {
 		} else {
 			hostname = args[0];
 		}
-
-		try {
+		Socket socker = new Socket(hostname, portNumber);
+		try {Thread outThread = new Thread(new Runnable() {
+                                 @Override
+                                 public void run(){
+                                    try{
+                                         InputStream serverInput = socker.getInputStream();
+                                         int character;
+                                         while((character = serverInput.read()) != -1){
+                                                 System.out.write((char)character);
+                                         }
+ 
+                                    } catch (IOException ce){
+                                         System.out.println("Exception: " + ce);
+                                    }
+                                 }
+                         });
 			// Create a one way connection to the server
-			Socket socker = new Socket(hostname, portNumber);
 			// Read the standard input stream
                         // What the user is inputting
                         // Reads a single char and returns the ASCII table value of that char
                         // Returns -1 if no character has been read(end i think)
-			
-			Thread inThread = new Thread(new Runnable() {
-				@Override
-				public void run(){
-				  try{
-					int character; 
-					OutputStream output = socker.getOutputStream();
-					while((character = System.in.read()) != -1){
-						output.write((byte)character);
-						output.flush();
-					}
-				    } catch (IOException ce) {
-		      			System.out.println("We were unable to connect to " + hostname);
-      					System.out.println("You should make sure the server is running.");
-				    }
-				}
-			});
-			Thread outThread = new Thread(new Runnable() {
-				@Override 
-				public void run(){
-				   try{	
-					InputStream serverInput = socker.getInputStream();
-					int character;
-					while((character = serverInput.read()) != -1){	
-						System.out.write((char)character);
-					}	
-				   } catch (IOException ce){
-					System.out.println("Something is very, very wrong..");
-				   }				
-				}
-			});
-			inThread.start();
 			outThread.start();
+			Thread inThread = new Thread(new Runnable() {
+                                @Override
+                                 public void run(){
+                                 try {
+                                         int character;
+                                         OutputStream output = socker.getOutputStream();
+                                         while((character = System.in.read()) != -1){
+                                                 output.write((byte)character);
+                                                 output.flush();
+                                         }
+                                         
+                                     } catch (IOException ce) {
+                                         System.out.println("We were unable to connect to " + hostname);
+                                         System.out.println("You should make sure the server is running.");
+                                     }
+                                 }
+ 
+                         });
+			inThread.start();
 			inThread.join();
+			socker.shutdownOutput();
 			outThread.join();
 			System.out.flush();
 			// Close the socket when we're done reading from it
+			socker.shutdownInput();
 			socker.close();
 
 			// Provide some minimal error handling.
